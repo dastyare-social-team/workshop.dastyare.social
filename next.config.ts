8b49dbbacf644b15d2f652d1dd6f464ba7495869
Omid Shabab <hey@omidshabab.com>
2026-07-21 07:51:17 -0700

    feat: add i18n, fonts, button component and not-found page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
+
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['*'],
   reactCompiler: true,
+  experimental: {
+    globalNotFound: true,
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
